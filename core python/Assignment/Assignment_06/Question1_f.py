a155d3398c635f5fe5818fe63c53b734ed1e8754
navalemanisha <https://github.com/navalemanisha/FBS.git>
2026-08-25 00:42:24 +0530

    Updated files

--- core python/Assignment/Assignment_06/Question1_f.py
+++ core python/Assignment/Assignment_06/Question1_f.py
@@ -5,20 +5,23 @@
 #   1 2 3 4 5 6 7
 # 1 2 3 4 5 6 7 8 9 
 
-# for i in range(1,6):
-#     for j in range(1,6-i):
-#         print(" ",end=" ")
-#     for j in range(1,2*i):
-#         print(j,end=" ")
-#     print()
-for i in range (1, 6):
-    k =1
-    for j in range (1, 6-i):
-        print(' ',end= ' ')
-    for j in range (1, i+1):
-        print(k, end= ' ')
-        k += 1
-    for j in range (1,i):
-        print(k,end= ' ')
-        k += 1
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(" ",end=" ")
+    for j in range(1,2*i):
+        print(j,end=" ")
     print()
+
+
+
+# for i in range (1, 6):
+#     k =1
+#     for j in range (1, 6-i):
+#         print(' ',end= ' ')
+#     for j in range (1, i+1):
+#         print(k, end= ' ')
+#         k += 1
+#     for j in range (1,i):
+#         print(k,end= ' ')
+#         k += 1
+#     print()
